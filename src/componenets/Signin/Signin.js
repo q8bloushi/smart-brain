ea27d2751fbdfd3661f6f9dfb8a172d06b8788c9
Mohammad Ahmed <71899593+q8bloushi@users.noreply.github.com>
2024-11-03 20:14:39 +0300

    Update Signin.js

--- src/componenets/Signin/Signin.js
+++ src/componenets/Signin/Signin.js
@@ -20,7 +20,7 @@ class Signin extends React.Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('https://sleepy-refuge-32199.herokuapp.com/signin', {
+        fetch('https://smart-brain-api-teoc.onrender.com/signin', {
             method: 'post',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
